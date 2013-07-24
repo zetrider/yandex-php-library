0170aa7c1bcc9f679189347353607561d2e75124
Anton Shevchuk <Anton.Shevchuk@gmail.com>
2013-07-24 13:09:20 +0300

    Added Exception structure for Pinger and SafeBrowsing Added requirements

---
+++ src/Yandex/SafeBrowsing/Exception/SafeBrowsingException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @namespace
+ */
+namespace Yandex\SafeBrowsing\Exception;
+
+use Yandex\Exception;
+
+/**
+ * Safe Browsing Exception
+ *
+ * @category Yandex
+ * @package  SafeBrowsing
+ *
+ * @author   Anton Shevchuk
+ * @created  24.07.13 11:20
+ */
+class SafeBrowsingException extends Exception
+{
+}
