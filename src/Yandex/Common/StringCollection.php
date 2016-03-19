38a38517e72e8090ccfde01ec44b9b2c56d2b7a7
vasilij <klunew@yandex.ru>
2016-03-19 19:00:01 +0300

    Fix code style in string collection class

--- src/Yandex/Common/StringCollection.php
+++ src/Yandex/Common/StringCollection.php
@@ -10,67 +10,67 @@ use Yandex\Common\Exception\InvalidArgumentException;
  * @category Yandex
  * @package  Common
  *
- * @author   Vasilii Zaluev
- * @created  17.03.16 22:31
+ * @author  Vasilii Zaluev
+ * @created 17.03.16 22:31
  */
 class StringCollection
 {
-   /**
+    /**
     * This string collection will convert to one string in get request by join
     *
     * @var string
     */
-   protected $collection = [];
+    protected $collection = [];
 
-   public function __construct($strings)
-   {
-      foreach($strings as $string){
-         if(!self::isString($string)){
-            throw new InvalidArgumentException('Argument must have only strings');
-         }
+    public function __construct($strings)
+    {
+        foreach ($strings as $string) {
+            if (!self::isString($string)) {
+                throw new InvalidArgumentException('Argument must have only strings');
+            }
 
-         $this->collection[] = $string;
-      }
-   }
+            $this->collection[] = $string;
+        }
+    }
 
-   /**
+    /**
     * @param $strings
     * @return null|self
     * @throws InvalidArgumentException
     */
-   public static function init($strings)
-   {
-      if (is_null($strings)) {
-         return null;
-      }
-
-      if (self::isString($strings)) {
-         return new static([$strings]);
-      }
-
-      if (is_array($strings)){
-         if(empty($strings)){
+    public static function init($strings)
+    {
+        if (is_null($strings)) {
             return null;
-         }
+        }
 
-         return new self($strings);
-      }
+        if (self::isString($strings)) {
+            return new static([$strings]);
+        }
 
-      throw new InvalidArgumentException('Argument must be string or string list, ' . gettype($strings) . ' received');
-   }
+        if (is_array($strings)) {
+            if (empty($strings)) {
+                return null;
+            }
 
-   public function getAll()
-   {
-      return implode(',', $this->collection);
-   }
+            return new self($strings);
+        }
 
-   private static function isString($string)
-   {
-      return is_string($string) || method_exists($string, '__toString');
-   }
+        throw new InvalidArgumentException('Argument must be string or string list ' . gettype($strings) . ' received');
+    }
 
-   public function asArray()
-   {
-      return $this->collection;
-   }
+    public function getAll()
+    {
+        return implode(',', $this->collection);
+    }
+
+    private static function isString($string)
+    {
+        return is_string($string) || method_exists($string, '__toString');
+    }
+
+    public function asArray()
+    {
+        return $this->collection;
+    }
 }
